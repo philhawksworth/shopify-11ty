15a2f03d85886eecc189d0bd61d824861bbac15d
Phil Hawksworth <phil@netlify.com>
2021-06-26 16:48:57 +0100

    Use shopify API helper util. Expose inventory

--- src/site/_data/products.js
+++ src/site/_data/products.js
@@ -1,43 +1,42 @@
-const fetch = require( 'node-fetch');
-const shopUrl = "https://netlify-demo.myshopify.com";
-const storefront_access_token = "b98313b8d60c1d61649070cc78cc41da"; // Safe to share. This is read-only. Not secret.
+const { postToShopify } = require('../../utils/postToShopify');
 
-// Assemble the graphql query to fetch info all teh products
-const query = `{
-  products(sortKey: TITLE, first: 100) {
-    edges {
-      node {
-        id
-        handle
-        description
-        title
-        images(first: 1) {
-          edges {
-            node {
-              src
-              altText
+module.exports = async () => {
+
+  const response = await postToShopify({
+    query: `{
+      products(sortKey: TITLE, first: 100) {
+        edges {
+          node {
+            id
+            handle
+            description
+            title
+            totalInventory
+            priceRange {
+              maxVariantPrice {
+                amount
+                currencyCode
+              }
+              minVariantPrice {
+                amount
+                currencyCode
+              }
+            }
+            images(first: 1) {
+              edges {
+                node {
+                  src
+                  altText
+                }
+              }
             }
           }
         }
       }
-    }
-  }
-}`;
-
-
-module.exports = async () => {
-
-  return await fetch(shopUrl + `/api/graphql`, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/graphql",
-      "X-Shopify-Storefront-Access-Token": storefront_access_token
-    },
-    body: query
-  })
-  .then(res => res.json())
-  .then(response => {
-    return response.data.products.edges;
+    }`,
+    variables: null
   });
+
+  return response.products.edges;
 
 };
---
+++ src/utils/postToShopify.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+
+require('dotenv').config();
+
+exports.postToShopify = async ({ query, variables }) => {
+  try {
+    const result = await fetch(process.env.SHOPIFY_API_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'X-Shopify-Storefront-Access-Token': process.env.SHOPIFY_STOREFRONT_API_TOKEN,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query, variables }),
+    }).then((res) => res.json());
+
+    if (!result || !result.data) {
+      return false;
+    }
+
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
