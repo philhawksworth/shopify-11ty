4f392411992bb3a187fc6222ee1e157499482a46
Phil Hawksworth <phil@netlify.com>
2021-07-01 14:39:47 +0100

    basic cart view

--- netlify/functions/cart-view.js
+++ netlify/functions/cart-view.js
@@ -17,14 +17,73 @@ exports.handler = async (event) => {
     return res.json()
   });
   
-  console.log(result);
+
+  const itemTotal = function(price, quantity) {
+    const totalPrice = Number(price) * Number(quantity)
+    return totalPrice.toFixed(2)
+  }
 
 
+  const cartItem = (cartId, item) => {
+    const displayTitleModifier = item.merchandise.title == "Default Title" ? "" : item.merchandise.title;
+    return `
+    <tr>
+    <td>${item.merchandise.product.title} ${displayTitleModifier}</td>
+    <td>${item.merchandise.priceV2.amount}</td>
+    <td>${item.quantity}</td>
+    <td>${ itemTotal(item.merchandise.priceV2.amount ,item.quantity) }</td>
+    <td>
+      <form action="/api/remove-from-cart" method="POST">
+        <input type="hidden" name="cartId" value="${cartId}">
+        <input type="hidden" name="lineId" value="${item.id}">
+        <input type="submit" value="Remove item">
+      </form>
+    </td>
+  </tr>
+  `};
+  
+  let items = "";
+    result.cart.lines.edges.forEach(item => {
+    items += cartItem(result.cart.id, item.node)
+  });
+
+
+  const pageTemplate = (data) => {return `
+  <html>
+    <head>
+      <link rel="stylesheet" href="/css/main.css">
+      <title>Your Cart</title>
+    </head>
+    <body>
+      <header>
+        <ul class="nav">
+          <li><a href="/">Browse the store</a></li>
+          <li><a href="/view-cart" class="cartLink">Cart</a></li>
+        </ul>
+      </header>
+      <div class="container">
+        
+      <table>
+        <thead>
+          <th>Item</th> 
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Total</th>
+          <th></th>
+        </thead>
+        <tbody>
+        ${data}
+        </tbody>
+        </table>
+      </div>
+      <script src="/js/post-to-cart.js"></script>
+    </body>
+    </html>
+    `};
     
-
   return {
     statusCode: 200,
-    body: `view of cart with ID: ${result.cart.id}, items: ${result.cart.lines.edges.length}`
+    body: pageTemplate(items)
   };
 
 }
--- src/js/post-to-cart.js
+++ src/js/post-to-cart.js
@@ -21,25 +21,22 @@ function getCartSummaryDetails() {
     postData('/api/get-cart', {
       'cartId': localStorage.getItem('shopifyCartId')
     })
-    .then(data => {  
-
-      console.log(data);
-      
+    .then(data => { 
       displayCartSummaryDetails(data.cart.lines.edges.length, data.cart.id);
     });
   }
 }
 
+
 // Update the UI with latest cart info
 function displayCartSummaryDetails(count, id) {
   const cartLink = document.getElementsByClassName('cartLink')[0];
   cartLink.innerHTML = `Cart (${count})`;
-
   cartLink.href = `/cart/?cartId=${id}`;
 }
 
 
-
+// dispatch post requests
 async function postData(url = '', data = {}) {
   const response = await fetch(url, {
     method: 'POST',
@@ -52,9 +49,8 @@ async function postData(url = '', data = {}) {
 }
 
 
-
+// Send an item to the cart API
 function postToCart(event) {
-  
   event.preventDefault();
   const inputs = event.target.elements; 
   const data = {
@@ -62,75 +58,11 @@ function postToCart(event) {
     itemId: inputs['merchandiseId'].value,
     quantity: inputs['quantity'].value,
   };
-  
   postData('/api/add-to-cart', data)
   .then(data => {
-    console.log(data);
-
     // persist that cartId for subsequent actions
     localStorage.setItem('shopifyCartId', data.id);
+    // update the cart ;abel in the navigation
     displayCartSummaryDetails(data.lines.edges.length, data.id);
   });
-
-
-    // get the data
-    // fetch('/api/add-to-post', data).then(function(response) {
-    //   return response.json();
-    // }).then(function(response) {
-    //  console.log("cart response", response);
-     
-    // });
-
-
-  // const cartResponse = await this.$http.$post('/api/add-to-cart', {
-  //   cartId: this.cartId,
-  //   itemId: this.selectedProductId,
-  //   quantity: 1,
-  // })
-  
-
-
 };
-
-
-
-// // Add a submit handler to any forms.
-// // Don't allow null submissions of required fields
-// (function(){
-//   var forms = document.querySelectorAll('form');
-//   if(forms.length == 0){ return;}
-
-//   // do this for all forms on the page
-//   for (var f = 0; f < forms.length; f++) {
-//     forms[f].addEventListener('submit', function(event) {
-//       event.preventDefault();
-//       var form = event.target;
-
-//       // reset any flags
-//       var flags = form.querySelectorAll('.needs-content');
-//       for (var f = 0; f < flags.length; f++) {
-//         flags[f].classList.remove('needs-content');
-//       }
-
-//       // flag any fields which are missing input
-//       var inputs = form.querySelectorAll('input');
-//       for (var i = 0; i < inputs.length; i++) {
-//         flagIfEmpty(inputs[i]);
-//       }
-//       var text = form.querySelectorAll('textarea');
-//       for (var t = 0; t < text.length; t++) {
-//         flagIfEmpty(text[t]);
-//       }
-
-//       // abort if there are flagged fields (other than the honeypot)
-//       // otherwise we can submit.
-//       flags = form.querySelectorAll('.needs-content');
-//       if(flags.length > 1) {
-//         return false;
-//       } else {
-//         form.submit();
-//       }
-
-//     }, false);
-//   }
-// })();
