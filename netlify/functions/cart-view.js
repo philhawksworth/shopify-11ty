73035279878dddbff89f7e4b6e78ace8bbc67414
Phil Hawksworth <phil@netlify.com>
2021-07-08 17:26:19 +0100

    styled cart view

--- netlify/functions/cart-view.js
+++ netlify/functions/cart-view.js
@@ -28,13 +28,20 @@ exports.handler = async (event) => {
 
   const cartItem = (cartId, item) => {
     const displayTitleModifier = item.merchandise.title == "Default Title" ? "" : item.merchandise.title;
-    return `
-    <tr>
-    <td>${item.merchandise.product.title} ${displayTitleModifier}</td>
-    <td>${item.merchandise.priceV2.amount}</td>
-    <td>${item.quantity}</td>
-    <td>${ itemTotal(item.merchandise.priceV2.amount ,item.quantity) }</td>
-    <td>
+    return ` <tr class="cart-table-row">
+    <td class="cart-table-cell">
+      <a href"=/products/${item.merchandise.product.handle}">
+        ${ item.merchandise.product.title } (${ item.merchandise.title })
+      </a>
+    </td>
+    <td class="cart-table-cell">
+      ${item.merchandise.priceV2.amount}
+    </td>
+    <td class="cart-table-cell">${ item.quantity }</td>
+    <td class="cart-table-cell">
+      ${ itemTotal(item.merchandise.priceV2.amount, item.quantity) }
+    </td>
+    <td class="cart-table-cell">
       <form action="/api/remove-from-cart" method="POST">
         <input type="hidden" name="cartId" value="${cartId}">
         <input type="hidden" name="lineId" value="${item.id}">
@@ -42,7 +49,7 @@ exports.handler = async (event) => {
       </form>
     </td>
   </tr>
-  `};
+`};
   
   let items = "";
     result.cart.lines.edges.forEach(item => {
@@ -53,43 +60,126 @@ exports.handler = async (event) => {
   const pageTemplate = (items, totals) => {return `
   <html>
   <head>
-  <link rel="stylesheet" href="/css/main.css">
-  <title>Your Cart</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <title>Your Cart</title>
   </head>
   <body>
+  
+    <header class="app-header">
+      <h1>Shoperoni</h1>
+      <nav class="main-nav">
+        <ul>
+          <li class="main-nav-item">
+            <a href="/">All<a>
+          </li>
+          <li class="main-nav-item">
+            <a href="/?type=cheese">Cheeses<a>
+          </li>
+          <li class="main-nav-item">
+            <a href="/?type=meat">Meats<a>
+          </li>
+          <li class="main-nav-item">
+            <a href="/?type=boards">Boards<a>
+          </li>
+          <li class="main-nav-item">
+            <div class="cart-size"></div>
+            <a href="/cart" class="cart cartLink">Shopping Cart</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
     <main>
-      <header>
-        <h1>Shoperoni</h1>
-        <h2>Shop for literally the best products in the world, right here.</h2>
-      </header>
-      <a class="home" href="/">Go back</a>
-      <table>
+      <div class="cart-page">
+      <article class="cart-page-content">
+        <h1>Your Cart</h1>
+        <div>
+        <table class="cart-table">
         <thead>
-          <th>Item</th> 
-          <th>Price</th>
-          <th>Quantity</th>
-          <th>Total</th>
-          <th></th>
+          <th class="cart-table-heading">Item</th>
+          <th class="cart-table-heading">Price</th>
+          <th class="cart-table-heading">Quantity</th>
+          <th class="cart-table-heading">Total</th>
+          <th class="cart-table-heading">Actions</th>
         </thead>
         <tbody>
         ${items}
-        <tr>
-          <td></td>
-          <td></td>
-          <td>Total</td>
-          <td>${totals.amount} ${totals.currencyCode}</td>
-        </tr>
+        
         </tbody>
         </table>
-      </main>
-      <script src="/js/shopping-ui.js"></script>
-    </body>
-    </html>
-    `};
+        <section class="cart-total">
+        <div class="cart-total-content">
+          <div class="cart-total-column">
+            <p>
+              <strong>Subtotal:</strong>
+            </p>
+            <p>Shipping:</p>
+            <p>Tax:</p>
+            <p>Total:</p>
+          </div>
+          <div class="cart-total-column">
+            <p>
+              <strong>${totals.subtotalAmount.amount} ${totals.totalAmount.currencyCode} </strong>
+            </p>
+            <p>Free Shipping</p>
+            <p>${totals.totalTaxAmount.amount} ${totals.totalAmount.currencyCode} </p>
+            <p>${totals.totalAmount.amount} ${totals.totalAmount.currencyCode} </p>
+          </div>
+        </div>
+      </section>
+        </div>
+      </article>
+    </div>
+    </main>
+    <footer>
+      <section class="testimonial">
+        <h2>
+          "The interplay of flavors between the cheese, meats and fruits is an
+          absolute delight."
+        </h2>
+        <p>Paul Hotcakes</p>
+      </section>
+      <section class="app-footer-links">
+        <ul>
+          <li>About</li>
+          <li>Company</li>
+          <li>Locations</li>
+          <li>Contact</li>
+          <li>Hours</li>
+        </ul>
+        <ul>
+          <li>Twitter</li>
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>LinkedIn</li>
+        </ul>
+        <div class="newsletter">
+          <h2 class="newsletter-title">Sign up for our newsletter:</h2>
+          <input
+            class="newsletter-input"
+            type="email"
+            placeholder="Enter your email"
+          />
+        </div>
+      </section>
+  
+      <div class="project-credit">
+        <p>
+          This project is
+          <a href="https://github.com/philhawksworth/shopify-11ty">open source on GitHub </a>,
+          hosted with <a href="https://bit.ly/2G29YwK">Netlify</a>, built with
+          <a href="https://11ty.dev/">Eleventy</a>
+          and made with 💚 by Phil Hawksworth (<a href="https://twitter.com/philhawksworth">@philhawksworth</a>)
+        </p>
+      </div>
+    </footer>
+    <script src="/js/shopping-ui.js"></script>
+  </body>
+  </html>
+  `};
     
   return {
     statusCode: 200,
-    body: pageTemplate(items, result.cart.estimatedCost.totalAmount)
+    body: pageTemplate(items, result.cart.estimatedCost)
   };
 
 }
